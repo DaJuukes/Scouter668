81cc8f5a4cf19fc39af5b67a32220ca3ab4eea6b
DaJuukes <dajuukes@gmail.com>
2018-11-18 11:16:55 -0800

    Remove check #2

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,12 +2,8 @@ module.exports = function (r) {
   let f = require('fs').readdirSync(`./src/controllers`)
   f.forEach((f) => {
     if (f === 'index.js') return
-    try {
-      console.log(`Adding ${f} to routes...`)
-      let file = require(`./${f}`)
-      r.use(`/${f.split('.')[0]}/`, file)
-    } catch (e) {
-      console.error(`Error loading ${f}:  ${e.stack}`)
-    }
+    console.log(`Adding ${f} to routes...`)
+    let file = require(`./${f}`)
+    r.use(`/${f.split('.')[0]}/`, file)
   })
 }
